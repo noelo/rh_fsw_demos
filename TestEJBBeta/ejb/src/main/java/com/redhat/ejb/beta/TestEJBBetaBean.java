5b0f6a41de3f6315af430699ed7845cf9b656bf4
Noel OConnor <noel.oconnor@gmail.com>
2014-05-09 11:25:57 +1000

    Adding @EJB code this time

--- TestEJBBeta/ejb/src/main/java/com/redhat/ejb/beta/TestEJBBetaBean.java
+++ TestEJBBeta/ejb/src/main/java/com/redhat/ejb/beta/TestEJBBetaBean.java
@@ -1,47 +1,55 @@
 package com.redhat.ejb.beta;
 
+import java.security.Principal;
+
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.annotation.security.RolesAllowed;
+
 import org.jboss.ejb3.annotation.SecurityDomain;
 
 /**
  * Session Bean implementation class TestEJBBetaBean
  */
 @Stateless
-@TestEJBQualifier
 @EJB(name = "java:global/TestEJBBetaBean", beanInterface = TestEJBBetaBeanLocal.class)
-//@SecurityDomain("other")
-//@RolesAllowed({ "friend" })
+@RolesAllowed({"friend"})
+@SecurityDomain("other")
 public class TestEJBBetaBean implements TestEJBBetaBeanLocal {
 
-    /**
-     * Default constructor. 
-     */
-    public TestEJBBetaBean() {
-    	System.out.println("TestEJBBetaBean constructor");
-    }
-    
-    @PostConstruct
-    private void postConstruct(){
-    	System.out.println("TestEJBBetaBean postConstruct");   	
-    }
+	@Resource
+	private SessionContext ctx;
+
+	/**
+	 * Default constructor.
+	 */
+	public TestEJBBetaBean() {
+		System.out.println("TestEJBBetaBean constructor");
+	}
+
+	@PostConstruct
+	private void postConstruct() {
+		System.out.println("TestEJBBetaBean postConstruct");
+	}
 
 	@Override
 	public String firstMethod(String in) {
-		System.out.println("FirstMethod invoked with "+in);
+		Principal principal = ctx.getCallerPrincipal();
+		System.out.println("FirstMethod invoked with " + in +" as "+principal.toString());
 		return in.toUpperCase();
 	}
 
 	@Override
 	public int secondMethod(int in) {
-		System.out.println("SecondMethod invoked with "+in);
-		return in*10;
+		System.out.println("SecondMethod invoked with " + in);
+		return in * 10;
 	}
 
 	@Override
 	public void thirdMethod(String in) {
-		System.out.println("ThirdMethod invoked with "+in);	
+		System.out.println("ThirdMethod invoked with " + in);
 	}
 }
--- policy-security-basic-propagate/src/main/java/org/switchyard/quickstarts/demo/policy/security/basic/propagate/BackEndServiceBean.java
+++ policy-security-basic-propagate/src/main/java/org/switchyard/quickstarts/demo/policy/security/basic/propagate/BackEndServiceBean.java
@@ -17,24 +17,73 @@ import static org.switchyard.policy.SecurityPolicy.AUTHORIZATION;
 import static org.switchyard.policy.SecurityPolicy.CLIENT_AUTHENTICATION;
 import static org.switchyard.policy.SecurityPolicy.CONFIDENTIALITY;
 
+import java.util.Hashtable;
+import java.util.Properties;
 
 //import org.apache.log4j.Logger;
 import org.switchyard.annotations.Requires;
 import org.switchyard.component.bean.Service;
-import com.redhat.ejb.beta.*;
-import javax.ejb.EJB;
 
-@Requires(security = {CONFIDENTIALITY, CLIENT_AUTHENTICATION, AUTHORIZATION})
+import com.redhat.ejb.beta.*;
+
+import javax.annotation.security.RunAs;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.jboss.security.auth.callback.UsernamePasswordHandler;
+
+@Requires(security = { CONFIDENTIALITY, CLIENT_AUTHENTICATION, AUTHORIZATION })
 @Service(BackEndService.class)
 public class BackEndServiceBean implements BackEndService {
 
-	@EJB(lookup="java:global/TestEJBBeta-ejb/TestEJBBetaBean") TestEJBBetaBeanLocal tb2;
+	 @EJB(lookup="java:global/TestEJBBeta-ejb/TestEJBBetaBean")
+	 TestEJBBetaBeanLocal tb2;
 
-    @Override
-    public String process(String in) {
-        System.out.println(":: BackEndService :: process => " + in);
-        tb2.firstMethod(in);
-        return "Processed by BackEndService: " + in;
-    }
+//	private TestEJBBetaBeanLocal ejbLookup() {
+//		TestEJBBetaBeanLocal ejbApp2 = null;
+//		try {
+//			Properties props = new Properties();
+//			props.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+//					"org.jboss.security.jndi.LoginInitialContextFactory");
+//			props.put(Context.SECURITY_PRINCIPAL, "kermit");
+//			props.put(Context.SECURITY_CREDENTIALS, "the-frog-1");
+//			props.setProperty(Context.SECURITY_PROTOCOL, "other");
+//			Context ctx = new InitialContext(props);
+//			ejbApp2 = (TestEJBBetaBeanLocal) ctx
+//					.lookup("java:global/TestEJBBeta-ejb/TestEJBBetaBean");
+//
+//		} catch (Throwable e) {
+//			e.printStackTrace();
+//		}
+//		return ejbApp2;
+//	}
+
+	@Override
+	public String process(String in) {
+		System.out.println(":: BackEndService :: process => " + in);
+//		TestEJBBetaBeanLocal tb2 = ejbLookup();
+		System.out.println("Looked up ejb now invoking");
+
+//		try {
+//			UsernamePasswordHandler handler = new UsernamePasswordHandler(
+//					"kermit", "the-frog-1");
+//			LoginContext lc;
+//			lc = new LoginContext("client-login", handler);
+//			lc.login();
+		
+			tb2.firstMethod(in);
+			
+//			lc.logout();
+//		} catch (LoginException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+		return "Processed by BackEndService: " + in;
+	}
 
 }
