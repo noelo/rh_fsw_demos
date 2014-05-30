43cf2f61a811fde7ac2856db883bceda6dad161a
Noel OConnor <noel.oconnor@gmail.com>
2014-05-30 11:47:46 +1000

    Update webapp with login config

--- camel-quartz-webapp/src/main/java/com/redhat/gps/test/ScheduleStart.java
+++ camel-quartz-webapp/src/main/java/com/redhat/gps/test/ScheduleStart.java
@@ -1,14 +1,18 @@
 package com.redhat.gps.test;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
 
+import javax.security.auth.Subject;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.security.jacc.PolicyContext;  
+import javax.security.jacc.PolicyContextException;
 import org.apache.log4j.*;
 
 /**
@@ -18,13 +22,13 @@ public class ScheduleStart extends HttpServlet {
 	private static final long serialVersionUID = 1934873489673124172L;
 	Logger logger = Logger.getLogger(ScheduleStart.class);
 
-    /**
-     * Default constructor. 
-     */
-    public ScheduleStart() {
-    	logger.info("ScheduleStart Constructor called");
+	/**
+	 * Default constructor.
+	 */
+	public ScheduleStart() {
+		logger.info("ScheduleStart Constructor called");
 
-    }
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
@@ -34,10 +38,24 @@ public class ScheduleStart extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		logger.info("doGet method called.............");
-	}
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
 
+	    out.println("<title>Invoked page</title>" +
+	       "<body bgcolor=FFFFFF>");
+	    
+	    out.println("<h1>Principal is "+request.getUserPrincipal().getName()+"</h1>");
+
+	    out.println("<h2>Test TEST Test</h2>");
+	    
+//	    final Principal userPrincipal = request.getUserPrincipal();
+//	    GenericPrincipal genericPrincipal = (Simpl) userPrincipal;
+//	    final String[] roles = genericPrincipal.getRoles();
+	    out.close();
+	}
 }
